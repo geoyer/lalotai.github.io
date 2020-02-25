60900d6116a6dbf71324916f19ada51429d77373
Lalotai <hi@lalot.ai>
2020-02-24 19:39:06 -0800

    new build

--- _next/static/zFaoqZFn1uTDAXVgur5m1/pages/_app.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{0:function(n,o,w){w("GcxT"),n.exports=w("nOHt")},GcxT:function(n,o,w){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return w("1TCz")}])}},[[0,1,2,0,3,4]]]);
